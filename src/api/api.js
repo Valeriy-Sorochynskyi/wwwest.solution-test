ed23181bd3c1b191d608e7399e5832ae977d27ab
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-03-25 01:23:35 +0200

    changed url to https

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line max-len
-const API_URL = 'http://newsapi.org/v2/top-headlines?country=ua&apiKey=1f247f15fab74ebb9e5898af7565afb8';
+const API_URL = 'https://newsapi.org/v2/top-headlines?country=ua&apiKey=1f247f15fab74ebb9e5898af7565afb8';
 
 export const loadNews = () => fetch(API_URL)
   .then(response => response.json())
